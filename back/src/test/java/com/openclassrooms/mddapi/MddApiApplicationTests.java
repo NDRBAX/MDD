1cb6b6dff1839fe28303e4142d257fff4354b84d
André Bacaliu <andre.bacaliu@icloud.com>
2025-03-18 13:41:58 +0100

    test(config): activate test profile in MddApiApplicationTests
    - Added @ActiveProfiles("test") annotation to MddApiApplicationTests
    class to ensure the "test" profile is active during test execution.

--- back/src/test/java/com/openclassrooms/mddapi/MddApiApplicationTests.java
+++ back/src/test/java/com/openclassrooms/mddapi/MddApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.openclassrooms.mddapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MddApiApplicationTests {
 
 	@Test
