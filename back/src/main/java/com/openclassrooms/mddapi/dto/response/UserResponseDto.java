db43a8ebc9a34afaae33cc089b50b07978d9310c
André Bacaliu <andre.bacaliu@icloud.com>
2025-04-01 10:56:33 +0200

    feat(dto): add UserResponseDto class
    - Added fields: `id`, `email`, `username`, and `subscriptions`.
    - Included Lombok annotations: `@Data`, `@Builder`,
    `@NoArgsConstructor`, and `@AllArgsConstructor` for boilerplate code
    reduction.

---
+++ back/src/main/java/com/openclassrooms/mddapi/dto/response/UserResponseDto.java
@@ -0,0 +1,19 @@
+package com.openclassrooms.mddapi.dto.response;
+
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDto {
+    private Long id;
+    private String email;
+    private String username;
+    private Set<SubscriptionResponseDto> subscriptions;
+}
