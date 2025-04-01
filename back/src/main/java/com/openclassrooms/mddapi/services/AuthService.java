bbb3797ff4a045d6ed6bd979aaa5fbb06363b734
André Bacaliu <andre.bacaliu@icloud.com>
2025-04-01 15:36:06 +0200

    feat(auth): enhance AuthService interface with new methods
    - Added `logout(String token)` method to handle user logout
    functionality.
    - Added `authenticateUser(String email, User loadedUser)` method for
    authenticating a user with email and user information.
    - Imported `User` model to support new authentication methods.

--- back/src/main/java/com/openclassrooms/mddapi/services/AuthService.java
+++ back/src/main/java/com/openclassrooms/mddapi/services/AuthService.java
@@ -4,10 +4,13 @@ import com.openclassrooms.mddapi.dto.request.UserLoginDto;
 import com.openclassrooms.mddapi.dto.request.UserRegistrationDto;
 import com.openclassrooms.mddapi.dto.response.AuthResponseDto;
 import com.openclassrooms.mddapi.dto.response.UserResponseDto;
+import com.openclassrooms.mddapi.models.User;
 
 
 public interface AuthService {
     AuthResponseDto register(UserRegistrationDto registrationDto);
     AuthResponseDto login(UserLoginDto loginDto);
     UserResponseDto getCurrentAuthenticatedUser();
+    void logout(String token);
+    AuthResponseDto authenticateUser(String email, User loadedUser);
 }
