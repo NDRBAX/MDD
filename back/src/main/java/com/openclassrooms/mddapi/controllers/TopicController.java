2d05634d460171d0e52379c289a8ee3298ec79cd
André Bacaliu <andre.bacaliu@icloud.com>
2025-04-01 12:36:19 +0200

    refactor(service): unify TopicService interface and implementation
    - Replaced ITopicService with TopicService to streamline service
    interface naming.
    - Updated TopicController to use the new TopicService interface.
    - Moved TopicService interface and updated the implementation class
    accordingly to the right package
    - Adjusted DTO import in TopicController to use TopicResponseDto.

--- back/src/main/java/com/openclassrooms/mddapi/controllers/TopicController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controllers/TopicController.java
@@ -7,22 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.openclassrooms.mddapi.dto.TopicDto;
-import com.openclassrooms.mddapi.services.ITopicService;
+import com.openclassrooms.mddapi.dto.response.TopicResponseDto;
+import com.openclassrooms.mddapi.services.TopicService;
 
 @RestController
 @RequestMapping("/api/topics")
 public class TopicController {
 
-    private final ITopicService topicService;
+    private final TopicService topicService;
 
-    public TopicController(ITopicService topicService) {
+    public TopicController(TopicService topicService) {
         this.topicService = topicService;
     }
 
     // get all topics
     @GetMapping
-    public ResponseEntity<List<TopicDto>> getAllTopics() {
+    public ResponseEntity<List<TopicResponseDto>> getAllTopics() {
         return ResponseEntity.ok(topicService.getAllTopics());
     }
 }
