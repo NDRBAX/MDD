e17999365c8df8174de46ab6988b9d9fc3576332
André Bacaliu <andre.bacaliu@icloud.com>
2025-04-01 15:34:13 +0200

    feat(dto): add new access and refresh tokens to UserResponseDto
    - Added `newAccessToken` and `newRefreshToken` fields to
    `UserResponseDto`.
    - These fields will store the new access token and refresh token for the
    user.

--- back/src/main/java/com/openclassrooms/mddapi/dto/response/UserResponseDto.java
+++ back/src/main/java/com/openclassrooms/mddapi/dto/response/UserResponseDto.java
@@ -15,5 +15,7 @@ public class UserResponseDto {
     private Long id;
     private String email;
     private String username;
+    private String newAccessToken;
+    private String newRefreshToken; 
     private Set<SubscriptionResponseDto> subscriptions;
 }
