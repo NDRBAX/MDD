0ba6d5d10c8a134230ea2ffc2878db12a4857f18
André Bacaliu <andre.bacaliu@icloud.com>
2025-04-01 16:36:13 +0200

    feat(models): add Subscription entity with unique constraints
    - Added `Subscription` entity to manage user subscriptions to topics.
    - Included unique constraint on `user_id` and `topic_id` to ensure a
    user can subscribe to a topic only once.
    - Configured `Subscription` with `user` and `topic` relationships.
    - Added `creationDate` field to track the creation time of each
    subscription.
    - Implemented `onCreate` method to set `creationDate` before persisting
    the entity.

---
+++ back/src/main/java/com/openclassrooms/mddapi/models/Subscription.java
@@ -0,0 +1,50 @@
+package com.openclassrooms.mddapi.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "subscriptions", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"user_id", "topic_id"})
+})
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "topic_id", nullable = false)
+    private Topic topic;
+    
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+    
+    @PrePersist
+    protected void onCreate() {
+        creationDate = LocalDateTime.now();
+    }
+}
