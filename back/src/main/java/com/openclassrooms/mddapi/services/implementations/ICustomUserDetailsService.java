834d187aea08e4e46d3d10dfac30619966c3f394
André Bacaliu <andre.bacaliu@icloud.com>
2025-04-01 15:48:14 +0200

    feat(service): implement custom UserDetailsService for user authentication
    - Added `ICustomUserDetailsService` class implementing
    `UserDetailsService` to provide custom user authentication logic.
    - Integrated with `UserRepository` to fetch user details based on email.
    - Included role-based authority assignment with default role as
    "ROLE_USER".
    - Utilized Lombok's `@RequiredArgsConstructor` for dependency injection.

---
+++ back/src/main/java/com/openclassrooms/mddapi/services/implementations/ICustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.openclassrooms.mddapi.services.implementations;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.openclassrooms.mddapi.models.User;
+import com.openclassrooms.mddapi.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ICustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Utilisateur non trouvé avec l'email : " + username));
+
+        List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+}
