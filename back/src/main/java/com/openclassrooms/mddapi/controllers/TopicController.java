0cd8d0571bfce49bfa1cfc41c76c29aadeb89169
André Bacaliu <andre.bacaliu@icloud.com>
2025-04-08 13:12:08 +0200

    refactor(controller): use Lombok @RequiredArgsConstructor for TopicController
    - Removed explicit constructor in `TopicController` and replaced it with
    Lombok's `@RequiredArgsConstructor` annotation to automatically generate
    the constructor.
    - This change reduces boilerplate code and leverages Lombok for better
    readability and maintainability.

--- back/src/main/java/com/openclassrooms/mddapi/controllers/TopicController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controllers/TopicController.java
@@ -10,17 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.openclassrooms.mddapi.dto.response.TopicResponseDto;
 import com.openclassrooms.mddapi.services.TopicService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/topics")
+@RequiredArgsConstructor
 public class TopicController {
 
     private final TopicService topicService;
 
-    public TopicController(TopicService topicService) {
-        this.topicService = topicService;
-    }
-
-    // get all topics
     @GetMapping
     public ResponseEntity<List<TopicResponseDto>> getAllTopics() {
         return ResponseEntity.ok(topicService.getAllTopics());
