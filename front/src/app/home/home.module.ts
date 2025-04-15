d353a651706f28dfc22457b2e7a85a40773f4f17
André Bacaliu <andre.bacaliu@icloud.com>
2025-04-15 16:12:11 +0200

    feat(home): enhance home module and component functionality
    Enhanced the Home module and component with the following updates:
    - Imported `SharedModule` into `HomeModule` to leverage shared
    functionalities.
    - Added navigation methods to the `HomeComponent` for redirecting to
    login and register pages.
    - Updated the HTML template to use `app-logo` component and bind
    navigation methods to buttons.
    - Refactored SCSS styles:
      - Reorganized `.login-buttons` within a `.home-page` wrapper.
    - Integrated responsive styles for mobile layouts using
    `responsive-mobile` mixin.

--- front/src/app/home/home.module.ts
+++ front/src/app/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent],
 })
 export class HomeModule { }
